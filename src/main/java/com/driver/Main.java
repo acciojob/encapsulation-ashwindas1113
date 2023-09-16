50e4bda8f5c9f4f354a0a45f6e776c80add562ae
Ashwin Das <ashwindas1113@gmail.com>
2023-09-16 08:32:29 -0500

    New changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly object = new RWOnly();
+        object.setter("Ashwin");
+        object.getter();
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    String getter(){
+        return this.name;
+    }
+    void setter(String input){
+        this.name = input;
+    }
 }

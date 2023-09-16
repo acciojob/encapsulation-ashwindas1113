17c5149198e9bf329427977aa24b93a46ec9e36b
Ashwin Das <ashwindas1113@gmail.com>
2023-09-16 08:36:35 -0500

    Updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         RWOnly object = new RWOnly();
-        object.setter("Ashwin");
-        object.getter();
+        object.setName("Ashwin");
+        object.getName();
     }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,10 +2,10 @@ package com.driver;
 
 public class RWOnly {
     private String name;
-    String getter(){
+    String getName(){
         return this.name;
     }
-    void setter(String input){
+    void setName(String input){
         this.name = input;
     }
 }
